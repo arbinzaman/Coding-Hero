cdb44db214eab3db20f381aac6992e84bb92e4fc
arbinzaman <arbinzmn@gmail.com>
2022-10-28 18:05:43 +0600

    Created whole FAQ Page

--- src/pages/Faq/Faq.js
+++ src/pages/Faq/Faq.js
@@ -3,7 +3,71 @@ import React from 'react';
 const Faq = () => {
     return (
         <div>
-            
+            <div className="dark:bg-gray-800 dark:text-gray-100">
+                <div className="container max-w-4xl px-10 py-6 mx-auto rounded-lg shadow-sm dark:bg-gray-900">
+                    <div className="flex items-center justify-between">
+                        <span className="text-sm dark:text-gray-400">OCT 28 2022</span>
+                      
+                    </div>
+                    <div className="mt-3">
+                        <a rel="noopener noreferrer" href="#" className="text-2xl font-bold hover:underline">How long does it take to build a website?</a>
+                        <p className="mt-2">This completely depends on what you need. It takes longer to build a 1000 page megasite than a smaller eight page brochure website.</p>
+                    </div>
+                   
+                </div>
+            </div>
+            <div className=" py-10 dark:bg-gray-800 dark:text-gray-100">
+                <div className="container max-w-4xl px-10 py-6 mx-auto rounded-lg shadow-sm dark:bg-gray-900">
+                    <div className="flex items-center justify-between">
+                        <span className="text-sm dark:text-gray-400">OCT 28 2022</span>
+                      
+                    </div>
+                    <div className="mt-3">
+                        <a rel="noopener noreferrer" href="#" className="text-2xl font-bold hover:underline">What size companies do you produce web designs for?</a>
+                        <p className="mt-2">We like to work with all sorts and sizes of businesses and individuals, and we bring the same skills used to support our celebrity and big business clients to the work we do for smaller clients. We’re just as keen to work with small businesses as we are large ones, and as a local firm we’re ideally placed to understand the needs of growing organisations.</p>
+                    </div>
+                   
+                </div>
+            </div>
+            <div className=" py-10 dark:bg-gray-800 dark:text-gray-100">
+                <div className="container max-w-4xl px-10 py-6 mx-auto rounded-lg shadow-sm dark:bg-gray-900">
+                    <div className="flex items-center justify-between">
+                        <span className="text-sm dark:text-gray-400">OCT 28 2022</span>
+                      
+                    </div>
+                    <div className="mt-3">
+                        <a rel="noopener noreferrer" href="#" className="text-2xl font-bold hover:underline">Do you redesign existing websites?</a>
+                        <p className="mt-2">We certainly can do! It’s important to approach website redesignssensitively to make sure your reasons for redesigning are valid. This will ensure the project is an overall success from your ROI point of view.</p>
+                    </div>
+                   
+                </div>
+            </div>
+            <div className=" py-10 dark:bg-gray-800 dark:text-gray-100">
+                <div className="container max-w-4xl px-10 py-6 mx-auto rounded-lg shadow-sm dark:bg-gray-900">
+                    <div className="flex items-center justify-between">
+                        <span className="text-sm dark:text-gray-400">OCT 28 2022</span>
+                      
+                    </div>
+                    <div className="mt-3">
+                        <a rel="noopener noreferrer" href="#" className="text-2xl font-bold hover:underline">Do you work internationally?</a>
+                        <p className="mt-2">Of course! With the advent of online conference tools such as Zoom, appear.in, and of course Skype, just to name a few, it makes working with businesses anywhere in the world straight forward enough.</p>
+                    </div>
+                   
+                </div>
+            </div>
+            <div className=" py-10 dark:bg-gray-800 dark:text-gray-100">
+                <div className="container max-w-4xl px-10 py-6 mx-auto rounded-lg shadow-sm dark:bg-gray-900">
+                    <div className="flex items-center justify-between">
+                        <span className="text-sm dark:text-gray-400">OCT 28 2022</span>
+                      
+                    </div>
+                    <div className="mt-3">
+                        <a rel="noopener noreferrer" href="#" className="text-2xl font-bold hover:underline">Will my website work on smart phones and tablets as well?</a>
+                        <p className="mt-2">It most certainly will! More people are accessing the web via small screens than ever, and the trend is likely to carry on. If your site doesn’t display or work properly on a mobile or tablet screen, you risk losing a big chunk of your potential audience. It means mobile responsive web design is essential these days. Every website we create is mobile responsive.</p>
+                    </div>
+                   
+                </div>
+            </div>
         </div>
     );
 };
