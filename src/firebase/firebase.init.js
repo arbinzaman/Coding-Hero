65541d3568dd04a10b6dfd5a8e47459ef4de6fc8
arbinzaman <arbinzmn@gmail.com>
2022-10-28 14:08:31 +0600

    Hide Firebase Api key

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -5,18 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC6OfXiOz0sN4RtY05MTcmVR-4QQEhMzCw",
-  authDomain: "coding-hero-325aa.firebaseapp.com",
-  projectId: "coding-hero-325aa",
-  storageBucket: "coding-hero-325aa.appspot.com",
-  messagingSenderId: "912662948316",
-  appId: "1:912662948316:web:41c6b091d19a4f46ee0719"
-  // apiKey:process.env.REACT_APP_apiKey,
-  // authDomain:process.env.REACT_APP_authDomain,
-  // projectId:process.env.REACT_APP_projectId,
-  // storageBucket:process.env.REACT_APP_storageBucket,
-  // messagingSenderId:process.env.REACT_APP_messagingSenderId,
-  // c:process.env.REACT_APP_appId,
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  c:process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
